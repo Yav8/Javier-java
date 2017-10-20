6e5fd3c60f176e9ae78dd8db8c4a34f57e8bfaf9
Yav <yavieron133@gmail.com>
2017-10-20 10:01:46 +0200

    Partiendo con el archivo que se tenía antes de comenzar la actividad.

---
+++ DAM1/Programación/javier.java
@@ -0,0 +1,10 @@
+public class javier{
+ public static void main(String args[]){
+
+	System.out.println("Javier de Cea Domínguez.");
+	System.out.println("Tengo 24 años");
+	System.out.println("Nací en Sevilla");
+	System.out.println("Mes actual: Septiembre");
+
+ }
+}
